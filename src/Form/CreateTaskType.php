bfc3a59cb9c5580bf9802e02cd85a31c5919c6e8
Kévy DARDOR <kevy.dardor@gmail.com>
2022-09-21 17:12:13 +0200

    Aperçu des différents types de champs 2/2

--- src/Form/CreateTaskType.php
+++ src/Form/CreateTaskType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Task;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,9 +13,13 @@ class CreateTaskType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name');
+        $builder->add('name', TextType::class, [
+            'required' => true,
+        ]);
         if ($options['hasDeadline']) {
-            $builder->add('deadline');
+            $builder->add('deadline', DateType::class, [
+                'widget' => 'single_text',
+            ]);
         }
     }
 
